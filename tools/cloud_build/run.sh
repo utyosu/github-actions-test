eafe4d13c9305afe6d88075e1f2dc348f2e7e7ed
utyosu <utyosu@gmail.com>
2020-01-25 12:16:53 +0900

    a

--- tools/cloud_build/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Rubyにパスを通す
-export PATH="/root/.rbenv/bin:/root/.rbenv/shims:$PATH"
-
-# Dockerfileがマルチレイヤのとき、MySQLを起動する前に以下コマンドの実行が必要
-find /var/lib/mysql -type f -exec touch {} \;
-
-. ${SCRIPT_PATH}
